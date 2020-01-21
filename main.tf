e8566c48f86dc6c5da001264a3d5a406c11a0cf0
Denislav Denov <denislavdenov@gmail.com>
2020-01-21 16:15:02 +0200

    adding code

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "aws_subnet" "tf_public_subnet" {
   vpc_id                  = aws_vpc.tf_vpc.id
   cidr_block              = var.public_cidrs[count.index]
   map_public_ip_on_launch = false
-  availability_zone       = data.aws_availability_zones.available.[count.index]
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
 
   tags = {
     Name = "tf_public_${count.index + 1}"
