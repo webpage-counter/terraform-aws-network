f16e619db18b2a58375ff380b847c4a1a89f4757
Denislav Denov <denislavdenov@gmail.com>
2020-01-21 16:24:44 +0200

    adding code again

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 #------Networking/outputs.tf
 
 output "public_subnets" {
-  value = aws_subnet.tf_public_subnet.id
+  value = aws_subnet.tf_public_subnet.*.id
 }
 
 output "private_subnets" {
@@ -9,11 +9,11 @@ output "private_subnets" {
 }
 
 output "public_sg" {
-  value = aws_security_group.tf_public_sg.id
+  value = aws_security_group.tf_public_sg.*.id
 }
 
 output "subnet_ips" {
-  value = aws_subnet.tf_public_subnet.cidr_block
+  value = aws_subnet.tf_public_subnet.*.cidr_block
 }
 
 output "vpc_id" {
